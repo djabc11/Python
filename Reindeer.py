000b28bb819426de362364b8591e2df081e5058e
djabc11 <davidaaronbc11@gmail.com>
2016-01-02 02:56:00 -0500

    1/1 changes
    Fixed trip calculation, added import from cvs

--- Reindeer.py
+++ Reindeer.py
@@ -21,6 +21,8 @@ x = [
 y = []
 x = [ [] ]
 
+
+
 """
 for line in open('Liberty Mutual/TrainingFeatures.csv'):
     line = line.split(',')
@@ -83,13 +85,17 @@ def calculateWRW(origin,destination,weight):
 
 def calculateWRWTrip(destinations):
     tripWRW = 0;
-    for i in range(0,len(destinations) - 1):
-        origin = destinations[0]
-        destination = destinations[1]
-        giftWRW = calculateWRW([origin[1],origin[2]],[destination[1],destination[2]],currentSleightWeight(destinations))
-        print(giftWRW)
+    destinationsCopy = destinations.copy();
+    for i in range(0,len(destinationsCopy) - 1):
+        origin = destinationsCopy[0]
+        destination = destinationsCopy[1]
+        #print(origin)
+        #print(destination)
+        giftWRW = calculateWRW([origin[1],origin[2]],[destination[1],destination[2]],currentSleightWeight(destinationsCopy))
+        
+        #print(giftWRW)
         tripWRW = tripWRW + giftWRW
-        del destinations[0]
+        del destinationsCopy[0]
     return tripWRW
 
 def currentSleightWeight(destinations):
@@ -114,17 +120,112 @@ def haversine(lat1, lon1, lat2, lon2):
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r
 
+def findNextDestination(destinations):
+    origin = destinations[0]
+    totalWeight = currentSleightWeight(destinations)
+    minValue = 1000000000
+    minID = 0
+    for i in range(1,len(destinations) - 1):
+        destination= destinations[i]
+        destinationWeight = destination[3]
+        thisValue = haversine(origin[1],origin[2],destination[1],destination[2])*(totalWeight-destinationWeight)
+        #print(destination[0])
+        #print("is")
+        #print(haversine(origin[1],origin[2],destination[1],destination[2]))
+        #print(totalWeight-destinationWeight)
+        if (thisValue < minValue):
+            minValue = thisValue
+            minID = destination[0]
 
+ 
+    return minID    
+
+
+def sortDestinations(destinations):
+    remainingDestinations = destinations.copy()
+    sortedDestinations = [[]]
+    sortedDestinations.append(remainingDestinations[0]) 
+    del sortedDestinations[0]
+    for j in range(0,len(remainingDestinations)-1):
+        nextOne = findNextDestination(remainingDestinations)
+        #print(remainingDestinations)
+        #print(nextOne)
+        indexOfNextOne = -1
+        for i in range(0,len(remainingDestinations)):
+            
+            if (remainingDestinations[i][0] == nextOne):
+                sortedDestinations.append(remainingDestinations[i])
+                indexOfNextOne = i
+        del remainingDestinations[0]
+        remainingDestinations.insert(0,remainingDestinations[indexOfNextOne-1])
+        del remainingDestinations[indexOfNextOne]
+        #del index 0
+        #move value to index 0
+
+        
+
+    #print(sortedDestinations)    
+    print(calculateWRWTrip(sortedDestinations))
+    """
+    take origin
+    iterate through options (ignoring last one which is final destination). for each one, take haversine*(totalWeight-weight)
+    """
+
+"""
 print(haversine(90,0,100,10))
 print(calculateWRW([90,0],[100,10],1))
+"""
 
 giftArray = [[0,90,0,0]]
-giftArray.append([77,24.2,76.12,50])
-giftArray.append([118,39.67,51.77,50])
+"""giftArray.append([77,24.2,76.12,50])
+giftArray.append([118,39.67,51.77,50])"""
+giftArray.append([30289,22.83434666, 10.56579953, 27.85789274])
+giftArray.append([43215,-0.911304709,    -73.38876968,    40.58210748])
+giftArray.append([156,-15.85388938,    144.6591108, 19.27233967])
+giftArray.append([5262,-38.13857064,    144.6125446, 50])
+giftArray.append([43405,   59.1551338,  92.94181219, 1])
+giftArray.append([83942,  -36.12365791,    -71.62390397,    22.19687077])
+giftArray.append([55569, 40.15935797, 49.96537485, 50])
+giftArray.append([51598,   -10.88303387,    -54.64391561,    20.51669253])
+giftArray.append([51026,   -77.18600036,    71.40373991, 9.321197229])
+giftArray.append([55912, 26.04471517, 3.130887811, 22.78740482])
+
 giftArray.append([0,90,0,0])
 
-
+"""
 print(calculateWRWTrip(giftArray))
 print(currentSleightWeight(giftArray))
+"""
+print(calculateWRWTrip(giftArray))
+print("how about now")
+print(sortDestinations(giftArray))
+
+
+giftArray2 = [[0,90,0,0,-1]]
+
+
+
+for line in open('gifts.csv'):
+    lineSplit = line.split(',')
+    lineSplit[0] = float(lineSplit[0])
+    lineSplit[1] = float(lineSplit[1])
+    lineSplit[2] = float(lineSplit[2])
+    lineSplit[3] = float(lineSplit[3])
+    giftArray2.append(lineSplit)
+
+
+del giftArray2[0] #remove dummy     
+#print(len(giftArray2))
+
+giftArray3 = giftArray2.copy()
+del giftArray3[20:len(giftArray3)]
+giftArray3.append([0,90,0,0])
+#print(len(giftArray3))
+
+print(giftArray3)
+print(calculateWRWTrip(giftArray3))
+print("how about now")
+print(sortDestinations(giftArray3))
+
 
 
